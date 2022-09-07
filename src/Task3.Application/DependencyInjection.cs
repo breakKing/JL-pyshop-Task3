1d8de989709b883647f04ec8366b7174fb91fa72
Daniil Dukhovnyi <dduhovny26@gmail.com>
2022-09-08 01:56:44 +0300

    Починка Lifetime у IEmissionService

--- src/Task3.Application/DependencyInjection.cs
+++ src/Task3.Application/DependencyInjection.cs
@@ -24,7 +24,7 @@ public static class DependencyInjection
 
         services.AddSingleton<IDateTimeService, DateTimeService>();
 
-        services.AddSingleton<IEmissionService, EmissionService>();
+        services.AddTransient<IEmissionService, EmissionService>();
 
         return services;
     }
